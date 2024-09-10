f2aa121874079ab72f62544add6c49187718bbc7
miguelperson <mi529437@ucf.edu>
2024-09-10 14:40:15 -0400

    was able to create registration page and successfully create account. New account is able to store into the mongoDB

--- app/src/main/java/com/example/esatestapp3/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/esatestapp3/ui/RegisterActivity.kt
@@ -1,10 +1,14 @@
 package com.example.esatestapp3.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.esatestapp3.MainActivity
 import com.example.esatestapp3.R
 
 class RegisterActivity : AppCompatActivity() {
@@ -17,5 +21,18 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val email = findViewById<EditText>(R.id.registerEmailText) // get email from registration page
+        val password = findViewById<EditText>(R.id.editTextTextPassword2) // get password
+        val regButton = findViewById<Button>(R.id.button3) // button thing
+        val backButton = findViewById<Button>(R.id.button4) // back to log in
+
+        backButton.setOnClickListener{ // going back to log in page
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        regButton.setOnClickListener{
+
+        } // end of event listener
     }
 }
